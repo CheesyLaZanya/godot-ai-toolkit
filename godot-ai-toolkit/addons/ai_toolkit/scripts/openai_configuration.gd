735293345d37afd8648c55ee900e6e08343e629e
A R <infinityBearDesigns@gmail.com>
2022-12-10 17:02:15 -0500

    Add Top P, Frequency Penalty, and Presence Penalty to dock.

--- godot-ai-toolkit/addons/ai_toolkit/scripts/openai_configuration.gd
+++ godot-ai-toolkit/addons/ai_toolkit/scripts/openai_configuration.gd
@@ -5,6 +5,12 @@ var temperature_input_node
 var temperature_slider_node
 var max_tokens_input_node
 var max_tokens_slider_node
+var top_p_input_node
+var top_p_slider_node
+var frequency_penalty_input_node
+var frequency_penalty_slider_node
+var presence_penalty_input_node
+var presence_penalty_slider_node
 
 
 func _ready():
@@ -12,6 +18,12 @@ func _ready():
 	temperature_slider_node = $"Temperature Panel/Temperature Slider"
 	max_tokens_input_node = $"Max Tokens Panel/Max Tokens Input"
 	max_tokens_slider_node = $"Max Tokens Panel/Max Tokens Slider"
+	top_p_input_node = $"Top P Panel/Top P Input"
+	top_p_slider_node = $"Top P Panel/Top P Slider"
+	frequency_penalty_input_node = $"Frequency Penalty Panel/Frequency Penalty Input"
+	frequency_penalty_slider_node = $"Frequency Penalty Panel/Frequency Penalty Slider"
+	presence_penalty_input_node = $"Presence Penalty Panel/Presence Penalty Input"
+	presence_penalty_slider_node = $"Presence Penalty Panel/Presence Penalty Slider"
 
 
 func update_slider_float_value(slider_node, value):
@@ -54,3 +66,40 @@ func _on_max_tokens_input_focus_exited():
 	update_slider_int_value(max_tokens_slider_node, input_text)
 
 
+func _on_top_p_slider_value_changed(value):
+	update_slider_input(top_p_input_node, value)
+
+
+func _on_top_p_input_text_submitted(new_text):
+	update_slider_float_value(top_p_slider_node, new_text)
+
+
+func _on_top_p_input_focus_exited():
+	var input_text = top_p_input_node.text
+	update_slider_float_value(top_p_slider_node, input_text)
+
+
+func _on_frequency_penalty_slider_value_changed(value):
+	update_slider_input(frequency_penalty_input_node, value)
+
+
+func _on_frequency_penalty_input_text_submitted(new_text):
+	update_slider_float_value(frequency_penalty_slider_node, new_text)
+
+
+func _on_frequency_penalty_input_focus_exited():
+	var input_text = frequency_penalty_input_node.text
+	update_slider_float_value(frequency_penalty_slider_node, input_text)
+
+
+func _on_presence_penalty_slider_value_changed(value):
+	update_slider_input(presence_penalty_input_node, value)
+
+
+func _on_presence_penalty_input_text_submitted(new_text):
+	update_slider_float_value(presence_penalty_slider_node, new_text)
+
+
+func _on_presence_penalty_input_focus_exited():
+	var input_text = presence_penalty_input_node.text
+	update_slider_float_value(presence_penalty_slider_node, input_text)
