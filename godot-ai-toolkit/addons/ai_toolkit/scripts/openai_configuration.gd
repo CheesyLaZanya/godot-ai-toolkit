acd8361a177c4f7d56fb92c7037ba2caee332452
A R <infinityBearDesigns@gmail.com>
2022-12-10 16:40:09 -0500

    Functionalize slider signals and add Max Tokens parameter.

--- godot-ai-toolkit/addons/ai_toolkit/scripts/openai_configuration.gd
+++ godot-ai-toolkit/addons/ai_toolkit/scripts/openai_configuration.gd
@@ -1,16 +1,56 @@
 @tool
 extends Panel
 
+var temperature_input_node
+var temperature_slider_node
+var max_tokens_input_node
+var max_tokens_slider_node
+
+
+func _ready():
+	temperature_input_node = $"Temperature Panel/Temperature Input"
+	temperature_slider_node = $"Temperature Panel/Temperature Slider"
+	max_tokens_input_node = $"Max Tokens Panel/Max Tokens Input"
+	max_tokens_slider_node = $"Max Tokens Panel/Max Tokens Slider"
+
+
+func update_slider_float_value(slider_node, value):
+	if value.is_valid_float():
+		slider_node.value = value.to_float()
+
+
+func update_slider_int_value(slider_node, value):
+	if value.is_valid_int():
+		slider_node.value = value.to_int()
+
+
+func update_slider_input(input_node, value):
+	input_node.text = str(value)
+	
 
 func _on_temperature_slider_value_changed(value):
-	$"Temperature Panel/Temperature Input".text = str(value)
+	update_slider_input(temperature_input_node, value)
 
 
 func _on_temperature_input_text_submitted(new_text):
-	if new_text.is_valid_float():
-		$"Temperature Panel/Temperature Slider".value = new_text.to_float()
+	update_slider_float_value(temperature_slider_node, new_text)
 
 
 func _on_temperature_input_focus_exited():
-	var input_text = $"Temperature Panel/Temperature Input".text
-	_on_temperature_input_text_submitted(input_text)
+	var input_text = temperature_input_node.text
+	update_slider_float_value(temperature_slider_node, input_text)
+
+
+func _on_max_tokens_slider_value_changed(value):
+	update_slider_input(max_tokens_input_node, value)
+
+
+func _on_max_tokens_input_text_submitted(new_text):
+	update_slider_int_value(max_tokens_slider_node, new_text)
+
+
+func _on_max_tokens_input_focus_exited():
+	var input_text = max_tokens_input_node.text
+	update_slider_int_value(max_tokens_slider_node, input_text)
+
+
