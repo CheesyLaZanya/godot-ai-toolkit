18b68d5d4f7d863dd6d2608acacc527f14052937
A R <infinityBearDesigns@gmail.com>
2022-12-10 20:26:50 -0500

    Add AI Provider selection.

---
+++ godot-ai-toolkit/addons/ai_toolkit/ai_selection.gd
@@ -0,0 +1,22 @@
+@tool
+extends Panel
+
+
+func _ready():
+	hide_all_panels()
+
+func hide_all_panels():
+	$"OpenAI Panel".hide()
+	$"Cohere Panel".hide()
+
+func _on_ai_selection_item_selected(index):
+	var selection = $"AI Selection Panel/AI Selection".get_item_text(index)
+
+	hide_all_panels()
+
+	if selection == "Choose an AI Provider":
+		hide_all_panels()
+	elif selection == "OpenAI":
+		$"OpenAI Panel".show()
+	elif selection == "co:here":
+		$"Cohere Panel".show()
