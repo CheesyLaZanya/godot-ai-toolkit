9b76411345b73d095c149033bf90d2a9c9c83040
Z L <admin@elementalzeal.com>
2022-12-11 12:55:53 -0500

    Add description boxes to sample selector scene.

--- godot-ai-toolkit/samples/sample_scripts/sample_scene_loader.gd
+++ godot-ai-toolkit/samples/sample_scripts/sample_scene_loader.gd
@@ -2,9 +2,19 @@ extends Control
 
 
 func _on_visual_novel_sample_button_button_up():
-	get_tree().change_scene_to_file("res://samples/sample_scenes/sample_visual_novel.tscn")
+	$SampleScreenPanel/RPGDescriptionPanel.hide()
+	$SampleScreenPanel/VisualNovelDescriptionPanel.show()
 
 
 func _on_rpg_sample_button_button_up():
-	get_tree().change_scene_to_file("res://samples/sample_scenes/sample_rpg.tscn")
+	$SampleScreenPanel/RPGDescriptionPanel.show()
+	$SampleScreenPanel/VisualNovelDescriptionPanel.hide()
 
+
+
+func _on_visual_novel_start_button_up():
+	get_tree().change_scene_to_file("res://samples/sample_scenes/sample_visual_novel.tscn")
+
+
+func _on_rpg_start_button_up():
+	get_tree().change_scene_to_file("res://samples/sample_scenes/sample_rpg.tscn")
